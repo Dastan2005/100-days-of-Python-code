a2c9993eadcb6a9d0dd9e306581bc6f10c63c1ee
Dastan2005 <mustapakulov2005@gmail.com>
2026-02-28 15:16:28 +0600

    Day 18, The Turtle and Graphical User interface(GUI)

--- day-18-start/main.py
+++ day-18-start/main.py
@@ -1,19 +1,22 @@
-from question_model import Question
-from data import question_data
-from quiz_brain import QuizBrain
-
-question_bank = []
-for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
-    new_question = Question(question_text, question_answer)
-    question_bank.append(new_question)
-
-quiz = QuizBrain(question_bank)
-quiz.next_question()
+from turtle import Turtle, Screen
+import random
 
 
-while quiz.still_has_question():
-    quiz.next_question()
-    print("You have completed the quiz")
-    print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+tim = Turtle()
+# tim.shape("turtle")
+tim.color("red")
+
+colors = ["Red", "Dark", "Blue", "Green", "Yellow", "Orange", "Violet", "Pink", "Purple"]
+
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+for shape_side_number in range(3, 11):
+    draw_shape(shape_side_number)
+    tim.color(random.choice(colors))
+
+screen = Screen()
+screen.exitonclick()
